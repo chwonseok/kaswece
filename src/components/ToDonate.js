c540babd316f64f762fa1c9419a68442f9f7d8d8
cws0316 <dev.chwonseok@gmail.com>
2021-10-12 15:03:52 +0900

    Added ToDonate.js with its design

--- src/components/ToDonate.js
+++ src/components/ToDonate.js
@@ -3,11 +3,15 @@ import './ToDonate.scss';
 export default function ToDonate() {
   return (
     <div className="container">
-      <div className="title">
-        <h1>후원하기</h1>
+      <div className="donate">
+        <div className="title">
+          <h1>후원하기</h1>
+        </div>
+        <h2>발도르프 교육을 후원하는 일, 지금 시작해보세요!</h2>
+        <button className="btn donate-btn" type="button">
+          후원하러 가기
+        </button>
       </div>
-      <span>발도르프 교육을 후원하는 일, 지금 시작해보세요!</span>
-      <button type="button">후원하러 가기</button>
     </div>
   );
 }
