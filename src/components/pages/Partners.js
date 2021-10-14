da363d3b88647c0c5fba707b54a0a8b5e6392bcc
cws0316 <dev.chwonseok@gmail.com>
2021-10-14 13:51:19 +0900

    Organized all data from Partner-list.js in Partners.js

--- src/components/pages/Partners.js
+++ src/components/pages/Partners.js
@@ -4,12 +4,26 @@ import partners from '../../db/Partner-list';
 import './Partners.scss';
 
 export default function Partners() {
-  console.log(partners);
   return (
     <>
       <HeadImg />
       <div className="title">
         <h1>회원 기관</h1>
+      </div>
+
+      <div className="container">
+        <div className="partners-box">
+          {partners.map((partner) => {
+            return (
+              <div
+                className="partner-box"
+                style={{ background: partner.color }}
+              >
+                <h2 className="partner-name">{partner.name}</h2>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
