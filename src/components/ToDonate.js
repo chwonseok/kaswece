8de7cd7021346af33c4ed3334498f3e36f0ecde5
cws0316 <dev.chwonseok@gmail.com>
2021-10-15 18:30:58 +0900

    Done Donate.js with its design

--- src/components/ToDonate.js
+++ src/components/ToDonate.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './ToDonate.scss';
 
 export default function ToDonate() {
@@ -8,9 +10,9 @@ export default function ToDonate() {
           <h1>후원하기</h1>
         </div>
         <h2>발도르프 교육을 후원하는 일, 지금 시작해보세요!</h2>
-        <button className="btn donate-btn" type="button">
+        <Link to="/donate" className="btn donate-btn" type="button">
           후원하러 가기
-        </button>
+        </Link>
       </div>
     </div>
   );
--- src/components/pages/Donate.js
+++ src/components/pages/Donate.js
@@ -1,3 +1,48 @@
+import HeadImg from './HeadImg';
+import bg from '../../images/donate-bg.jpeg';
+
+import './Donate.scss';
+
 export default function Donate() {
-  return <h1>Donate!</h1>;
+  return (
+    <>
+      <HeadImg title={'후원하기'} bg={bg} />
+      <div className="container">
+        <div className="title">
+          <h1>후원 계좌</h1>
+        </div>
+        <div className="centerize">
+          <p>한국 123123-12-123-123 한국발도르프영유아교육연대</p>
+
+          <div className="title">
+            <h1>후원 문의</h1>
+          </div>
+
+          <p>Tel: 123-1234-1234</p>
+          <br />
+          <p>Fax: 12-123-1234</p>
+          <br />
+          <p>E-mail: abc@abcd.com</p>
+          <br />
+
+          <div>
+            <br />
+            <br />
+            <p>
+              본 단체는 국제발도르프교육 그룹과 연대 협력을 통해 교육의 질을
+              보호하고 지원할 수 있는 후원사업을 아래와 같이 진행하고자 합니다.
+            </p>
+            <br />
+            <br />
+            <p>
+              1. 발도르프 교사교육 및 인재양성을 위한 지원 사업
+              <br /> 2. 발도르프 교육 실천현장 지원 사업
+              <br />
+              3. 발도르프교사 및 연구 지원 사업
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
