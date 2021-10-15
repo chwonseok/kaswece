1ee8b64084ff1643f6ee089197b863a6906a39ff
cws0316 <dev.chwonseok@gmail.com>
2021-10-15 15:13:46 +0900

    Done Toys.js with its design

--- src/components/pages/Toys.js
+++ src/components/pages/Toys.js
@@ -1,6 +1,8 @@
 import HeadImg from './HeadImg';
 import data from '../../db/ItemsList';
 
+import './Toys.scss';
+
 export default function Toys() {
   return (
     <>
@@ -8,7 +10,26 @@ export default function Toys() {
       <div className="title">
         <h1>놀잇감</h1>
       </div>
-      <div className="toys-box container">h1</div>
+      <div className="container">
+        <div className="toys-box">
+          {data
+            .filter((cur) => cur.tag === 'toys')
+            .map((toy) => {
+              return (
+                <div key={toy.id} className="toy-box">
+                  <div className="toy-img">
+                    <img
+                      src={process.env.PUBLIC_URL + `/images/items/${toy.link}`}
+                      alt={toy.name}
+                    />
+                  </div>
+                  <div className="toy-name">{toy.name}</div>
+                  <div className="toy-price">{toy.price}원</div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
   );
 }
