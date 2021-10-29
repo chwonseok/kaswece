129db841699d9df831d37c1a75a2c2828dabda51
cws0316 <dev.chwonseok@gmail.com>
2021-10-29 15:31:16 +0900

    Add: plus and minus function on item amount - used useState to get total amount and price in ItemDetailJs

--- src/components/pages/ItemDetail.js
+++ src/components/pages/ItemDetail.js
@@ -1,10 +1,27 @@
 import './ItemDetail.scss';
 import data from '../../db/ItemsList';
+import { useState } from 'react';
 
 export default function ItemDetail(props) {
+  const [quantity, setQuantity] = useState(1);
   const itemId = props.match.params.id;
   const item = data[itemId];
-  console.log(item);
+  const [price, setPrice] = useState(item.price);
+
+  const handlePlus = () => {
+    setQuantity(quantity + 1);
+    const totalPrice =
+      (quantity + 1) * parseInt(item.price.split(',').join(''));
+    setPrice(totalPrice.toLocaleString());
+  };
+
+  const handleMinus = () => {
+    if (quantity < 2) return;
+    setQuantity(quantity - 1);
+    const totalPrice =
+      (quantity - 1) * parseInt(item.price.split(',').join(''));
+    setPrice(totalPrice.toLocaleString());
+  };
 
   return (
     <div className="container">
@@ -22,7 +39,36 @@ export default function ItemDetail(props) {
             <span>배송비: </span> 무료
           </h3>
           {/* 수량 function 추가로 넣기 */}
-          <div className="item-quantity"></div>
+          <div className="item-quantity">
+            <div
+              onClick={() => {
+                handlePlus();
+              }}
+              className="quantity-btn plus"
+            >
+              <span>+</span>
+            </div>
+            <div className="amount">
+              <span>{quantity}</span>
+            </div>
+            <div
+              onClick={() => {
+                handleMinus();
+              }}
+              className="quantity-btn minus"
+            >
+              <span>-</span>
+            </div>
+          </div>
+          <div className="item-border"></div>
+          <div className="total-amount">
+            <h3>주문 수량</h3>
+            <h4>{quantity}개</h4>
+          </div>
+          <div className="total-price">
+            <h3>총 상품 금액</h3>
+            <h4>{price}원</h4>
+          </div>
           <button className="btn item-btn">구매하기</button>
         </div>
       </div>
